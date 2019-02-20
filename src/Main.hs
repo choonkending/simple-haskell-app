9e7b807e8b81009e8173babf84dc56d9ab574243
Ken Ding <ken.ding@rea-group.com>
2019-02-20 16:05:23 +1100

    Create a simple server which returns hello world
    - Used OverloadedStrings to convert List of Char to ByteString

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+import Network.Wai(Application, responseLBS)
+import Network.Wai.Handler.Warp(run)
+import Network.HTTP.Types.Status(status200)
+
+app :: Application
+app req respond = respond (responseLBS status200 [] "Hello World")
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  run 8080 app
