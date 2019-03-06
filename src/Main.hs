7b69b0c6b5bbc7dcd50dd591311671311face06e
Ken Ding <ken.ding@rea-group.com>
2019-03-06 16:27:23 +1100

    Parse and return query string

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Network.Wai(Application, responseLBS)
+import Network.Wai(Application, responseLBS, queryString)
 import Network.Wai.Handler.Warp(run)
 import Network.HTTP.Types.Status(status200)
+import Control.Monad(join)
+import Data.Maybe(fromMaybe)
+import Data.ByteString.Lazy(fromStrict)
 
 app :: Application
-app req respond = respond (responseLBS status200 [] "Hello World")
+app req respond = respond (responseLBS status200 [] body) where
+  query = queryString req
+  message = join (lookup "message" query)
+  body = fromStrict (fromMaybe "empty" message)
 
 main :: IO ()
 main = do
