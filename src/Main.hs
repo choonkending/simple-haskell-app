a279e4c1bebefc55c1e5020ddd7920a7148f8347
Ken Ding <ken.ding@rea-group.com>
2019-03-06 17:01:26 +1100

    Accept form values and return them

--- src/Main.hs
+++ src/Main.hs
@@ -2,16 +2,30 @@
 module Main where
 import Network.Wai(Application, responseLBS, queryString)
 import Network.Wai.Handler.Warp(run)
-import Network.HTTP.Types.Status(status200)
+import Network.Wai.Parse(parseRequestBodyEx, defaultParseRequestBodyOptions, Param, FileInfo)
+import Network.HTTP.Types.Status(status200, status400)
 import Control.Monad(join)
 import Data.Maybe(fromMaybe)
 import Data.ByteString.Lazy(fromStrict)
+import Data.ByteString(ByteString)
+import Data.Monoid((<>))
 
 app :: Application
-app req respond = respond (responseLBS status200 [] body) where
-  query = queryString req
-  message = join (lookup "message" query)
-  body = fromStrict (fromMaybe "empty" message)
+app req respond = do
+  (params, _) <- parseRequestBodyEx defaultParseRequestBodyOptions backend req
+  let name = combineNames params
+  case name of
+    Just body -> respond (responseLBS status200 [] (fromStrict body))
+    Nothing -> respond (responseLBS status400 [] "no")
+
+backend :: ByteString -> FileInfo () -> IO ByteString -> IO a
+backend _ _ _ = error "dont want file upload"
+
+combineNames :: [Param] -> Maybe ByteString
+combineNames params = do
+  firstName <- lookup "firstName" params
+  lastName <- lookup "lastName" params
+  pure (firstName <> " " <> lastName)
 
 main :: IO ()
 main = do
