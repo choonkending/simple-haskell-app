ad276969a2398678b9743c0c90de5ea710ee1ea8
Ken Ding <ken.ding@rea-group.com>
2019-03-13 15:33:40 +1100

    Create middleware to handle name request

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Network.Wai(Application, responseLBS, queryString)
+import Network.Wai(Application, Middleware, requestMethod, responseLBS, queryString)
 import Network.Wai.Handler.Warp(run)
 import Network.Wai.Parse(parseRequestBodyEx, defaultParseRequestBodyOptions, Param, FileInfo)
-import Network.HTTP.Types.Status(status200, status400)
+import Network.HTTP.Types.Status(status200, status400, status404)
 import Control.Monad(join)
 import Data.Maybe(fromMaybe)
 import Data.ByteString.Lazy(fromStrict)
@@ -11,12 +11,28 @@ import Data.ByteString(ByteString)
 import Data.Monoid((<>))
 
 app :: Application
-app req respond = do
+app = handleNameRequestMiddleware notFound
+
+notFound :: Application
+notFound _ respond =  respond (responseLBS status404 [] "Not Found")
+
+handleNameRequestMiddleware :: Middleware
+handleNameRequestMiddleware app1 = app2 where
+  app2 req respond = if isNameRequest req
+    then handleNameRequest req respond
+    else app1 req respond
+  isNameRequest req =
+    if requestMethod req == "POST"
+      then True
+      else False
+
+handleNameRequest :: Application
+handleNameRequest req respond = do
   (params, _) <- parseRequestBodyEx defaultParseRequestBodyOptions backend req
   let name = combineNames params
   case name of
     Just body -> respond (responseLBS status200 [] (fromStrict body))
-    Nothing -> respond (responseLBS status400 [] "no")
+    Nothing -> respond (responseLBS status400 [] "no names")
 
 backend :: ByteString -> FileInfo () -> IO ByteString -> IO a
 backend _ _ _ = error "dont want file upload"
