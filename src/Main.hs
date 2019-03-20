24e36540548077e8348915906a3e12bb36ac055e
Ken Ding <ken.ding@rea-group.com>
2019-03-20 15:40:59 +1100

    Attempt different ifRequest for middleware composition

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Network.Wai(Application, Middleware, requestMethod, pathInfo, responseLBS, queryString)
+import Network.Wai(Application, Middleware, Request, ifRequest, requestMethod, pathInfo, responseLBS, queryString)
 import Network.Wai.Handler.Warp(run)
 import Network.Wai.Parse(parseRequestBodyEx, defaultParseRequestBodyOptions, Param, FileInfo)
 import Network.HTTP.Types.Status(status200, status400, status404)
@@ -13,13 +13,35 @@ import Data.Monoid((<>))
 import Debug.Trace(trace)
 
 app :: Application
-app = handleNameRequestMiddleware notFound
+app = z notFound
 
 notFound :: Application
 notFound _ respond =  respond (responseLBS status404 [] "Not Found")
 
 handleNameRequestMiddleware :: Middleware
-handleNameRequestMiddleware app1 = app2 where
+handleNameRequestMiddleware _ = handleNameRequest
+
+z :: Middleware
+z = ifRequest3 isNameRequest handleNameRequest
+
+ifRequest2 :: (Request -> Bool) -> Middleware -> Middleware -> Middleware
+ifRequest2 pred success fail app req = if pred req
+  then success app req
+  else fail app req
+
+ifRequest3 :: (Request -> Bool) -> Application -> Middleware
+ifRequest3 pred app1 app2 req = if pred req
+  then app1 req
+  else app2 req
+
+isNameRequest :: Request -> Bool
+isNameRequest req =
+  case (requestMethod req, pathInfo req) of
+    (method, ["names"]) | method == methodPost -> True
+    _ -> False
+
+handleNameRequestMiddleware2 :: Middleware
+handleNameRequestMiddleware2 app1 = app2 where
   app2 req respond = if isNameRequest req
     then handleNameRequest req respond
     else app1 req respond
